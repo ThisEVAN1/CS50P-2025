3f533d553ad9a5b4719bc0095b3eb63da3d5d275
ThisEVAN1 <evanxu365@gmail.com>
2025-09-05 18:42:05 -0700

    faces

---
+++ faces/faces.py
@@ -0,0 +1,10 @@
+def main():
+    # Code here
+    string = input('Type your string: ')
+    print(string.replace(':(', '🙁').replace(':)', '🙂'))
+
+#def convert():
+    # Convert the string
+
+
+main()
