7b3ad3ed60900cb1152787ecb21171ccf93c4a11
ThisEVAN1 <evanxu365@gmail.com>
2025-09-06 14:58:28 -0700

    twttr

---
+++ week1/twttr/twttr.py
@@ -0,0 +1,6 @@
+text = input('Input: ')
+
+# Replace all the vowels with nothing
+new_text = text.replace('A', '').replace('E', '').replace('I', '').replace('O', '').replace('U', '').replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+
+print(new_text)
