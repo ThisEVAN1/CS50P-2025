f8b6cbcd0abdc01258ee9950d13dd3fa2e2222ad
ThisEVAN1 <evanxu365@gmail.com>
2025-09-07 15:49:42 -0700

    taqueria

---
+++ week3/taqueria/taqueria.py
@@ -0,0 +1,27 @@
+total_price = 0
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+while True:
+    try:
+        item = input('Item: ').title()
+        item_price = menu.get(item)
+        total_price += item_price
+        print(f'Total: ${total_price:.2f}')
+    except EOFError:
+        print()
+        break
+    except KeyError:
+        ...
+    except TypeError:
+        ...
