28512673460c080a1fede0a5528a1e44bf5187c5
ThisEVAN1 <evanxu365@gmail.com>
2025-10-02 20:25:22 -0700

    Created a countdown

--- week9/project/project.py
+++ week9/project/project.py
@@ -1,19 +1,23 @@
 import csv
-import argparse
 from tabulate import tabulate
 import sys
 import time
+import re
 
 
 class Quiz:
     data = []
 
-    def __init__(self, file):
+    def __init__(self, file, duration):
         self._file = file
+        self._duration = duration
         if not file.endswith('.csv'):
             raise FileNotFoundError('Didn\'t find a csv file')
 
     def __str__(self):
+        '''
+        Return the data dict
+        '''
         return str(self.data)
 
     def create_dict(self):
@@ -32,17 +36,30 @@ class Quiz:
             for row in reader:
                 # Append the question and answer to the dict and also set answered to false
                 Quiz.data.append({
-                    headers[0]: row[headers[0]],
-                    headers[1]: row[headers[1]],
+                    headers[0]: row[headers[0]].strip().lower(),
+                    headers[1]: row[headers[1]].strip().lower(),
                     'answered': False
                 })
          
 
-
 def main():
-    quiz = Quiz('example.csv')
+    
+    quiz = Quiz('example.csv', )
     quiz.create_dict()
     print(quiz)
+
+
+def countdown():
+    time_limit = int(input('Time limit (example: 3:25 for 3 min and 25 secs): ').strip().lower())
+    if time_limit == 'inf':
+        return 999999
+    elif match := re.search(r'(\d)*:(0\d|\d{2})', time_limit):
+        min = eval(match.group(1))
+        sec = eval(match.group(2))
+
+        return min * 60 + sec
+    else:
+        raise ValueError('Invalid format')
 
 
 if __name__ == '__main__':
